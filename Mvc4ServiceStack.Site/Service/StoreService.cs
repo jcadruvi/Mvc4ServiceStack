3097011c598fe2f270b82732ca365a04fd53a2a1
Josh Cadruvi <jcadruvi@mobitor.com>
2013-05-29 17:14:08 -0700

    Added StoreService.cs.

---
+++ Mvc4ServiceStack.Site/Service/StoreService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack.ServiceHost;
+
+namespace Mvc4.ServiceStack.Service
+{
+    /// <summary>
+    /// Define your ServiceStack web service request (i.e. the Request DTO).
+    /// </summary>    
+    public class Store
+    {
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// Define your ServiceStack web service response (i.e. Response DTO).
+    /// </summary>
+    public class StoreResponse
+    {
+        public string Result { get; set; }
+    }
+
+    /// <summary>
+    /// Create your ServiceStack web service implementation.
+    /// </summary>
+    public class StoreService : IService
+    {
+        public object Any(Store request)
+        {
+            //Looks strange when the name is null so we replace with a generic name.
+            var name = request.Name ?? "John Doe";
+            return new StoreResponse { Result = "Hello, " + name };
+        }
+    }
+}
