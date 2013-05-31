912ccd92dca5b08d1c64563aa166dfb1789ceb23
Josh Cadruvi <jcadruvi@mobitor.com>
2013-05-31 14:53:47 -0700

    Moved the get methods in the OperationService to the StoreService.

--- Mvc4ServiceStack.Service/Service/StoreService.cs
+++ Mvc4ServiceStack.Service/Service/StoreService.cs
@@ -27,12 +27,64 @@ namespace Mvc4.ServiceStack.Service
             return Stores();
         }
 
+        public IEnumerable<KeyValuePair<string, string>> Get(OrgLevelRequest request)
+        {
+            ICollection<KeyValuePair<string, string>> response = new Collection<KeyValuePair<string, string>>();
+
+            response.Add(new KeyValuePair<string, string>("1", "Bay Area"));
+            response.Add(new KeyValuePair<string, string>("2", "Los Angelos"));
+            response.Add(new KeyValuePair<string, string>("3", "San Diego"));
+
+            return response;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> Get(RetailerRequest request)
+        {
+            ICollection<KeyValuePair<string, string>> response = new Collection<KeyValuePair<string, string>>();
+            response.Add(new KeyValuePair<string, string>("1", "Best Buy"));
+            response.Add(new KeyValuePair<string, string>("2", "Frys"));
+            response.Add(new KeyValuePair<string, string>("3", "Wal Mart"));
+            response.Add(new KeyValuePair<string, string>("4", "Target"));
+            response.Add(new KeyValuePair<string, string>("5", "Safeway"));
+            response.Add(new KeyValuePair<string, string>("6", "Knob Hill"));
+            response.Add(new KeyValuePair<string, string>("7", "Luckys"));
+            return response;
+        }
+
         public StoreResponse Get(StoreRequest store)
         {
             return (from s in Stores()
                     where s.Id == store.Id
                     select s).First();
         }
+
+        public IEnumerable<KeyValuePair<string, string>> Get(SubOrgLevelRequest request)
+        {
+            ICollection<KeyValuePair<string, string>> response = new Collection<KeyValuePair<string, string>>();
+
+            switch (request.OrgLevelId)
+            {
+                case 1:
+                    response.Add(new KeyValuePair<string, string>("1", "San Fransisco"));
+                    response.Add(new KeyValuePair<string, string>("2", "San Jose"));
+                    response.Add(new KeyValuePair<string, string>("3", "Oakland"));
+                    break;
+                case 2:
+                    response.Add(new KeyValuePair<string, string>("4", "Los Angelos"));
+                    response.Add(new KeyValuePair<string, string>("5", "Glendale"));
+                    response.Add(new KeyValuePair<string, string>("6", "Simi Valley"));
+                    break;
+                case 3:
+                    response.Add(new KeyValuePair<string, string>("7", "San Diego"));
+                    response.Add(new KeyValuePair<string, string>("8", "Oceanside"));
+                    response.Add(new KeyValuePair<string, string>("9", "Escondido"));
+                    break;
+            }
+
+            return response;
+        }
+
+
 
         public object Post(StoreRequest store)
         {
